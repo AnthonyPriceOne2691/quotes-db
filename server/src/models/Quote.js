eb52a6a9c02c4afcbeaa860434187956ba58e249
AnthonyPriceOne2691 <anthonypriceone@gmail.com>
2024-10-12 19:33:39 +0300

    add limit and offset for quotes route and add validation, add array of categories to the each quote in API

--- server/src/models/Quote.js
+++ server/src/models/Quote.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/database');
 const QuoteCategory = require('./QuoteCategory');
 const Category = require('./Category');
 
-const Quote = sequelize.define('Quote', {
+const fields = {
   text: {
     type: DataTypes.TEXT,
     allowNull: false,
@@ -11,6 +11,24 @@ const Quote = sequelize.define('Quote', {
   author: {
     type: DataTypes.STRING,
   },
+};
+
+const afterFind = (results) => {
+  if (results) {
+    const quotes = Array.isArray(results) ? results : [results];
+    quotes.forEach((quote) => {
+      if (quote.Categories) {
+        quote.dataValues.categories = quote.Categories.map(
+          (category) => category.name
+        );
+        delete quote.dataValues.Categories;
+      }
+    });
+  }
+};
+
+const Quote = sequelize.define('Quote', fields, {
+  hooks: { afterFind },
 });
 
 Quote.belongsToMany(Category, { through: QuoteCategory });
--- server/src/routes/quotesRoutes.js
+++ server/src/routes/quotesRoutes.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const { query, param } = require('express-validator');
+const validationErrorHandler = require('../middlewares/validationErrorHandler');
 const router = express.Router();
 const quotesController = require('../controllers/quotesController');
 
-router.get('/', quotesController.getAllQuotes);
+router.get(
+  '/',
+  [
+    query('limit').optional().trim().isInt({ min: 1, max: 50 }),
+    query('offset').optional().trim().isInt({ min: 0 }),
+  ],
+  validationErrorHandler,
+  quotesController.getAllQuotes
+);
 
-router.get('/:id', quotesController.getQuoteById);
+router.get(
+  '/:id',
+  [param('id').trim().isInt({ min: 1 })],
+  validationErrorHandler,
+  quotesController.getQuoteById
+);
 
 module.exports = router;
