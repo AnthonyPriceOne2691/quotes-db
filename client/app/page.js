51f6a7497531bbaf212bca1ac308b15425bcef51
AnthonyPriceOne2691 <anthonypriceone@gmail.com>
2024-10-29 00:52:20 +0300

    Button to get another 10 random quotes

--- client/app/page.js
+++ client/app/page.js
@@ -2,18 +2,22 @@
 
 import { useEffect, useState } from 'react';
 
+const RANDOM_QUOTES_URL = 'http://localhost:3000/quotes/random?limit=10';
+
 export default function Home() {
   const [quotes, setQuotes] = useState([]);
 
-  useEffect(() => {
-    const fetchQuotes = async () => {
-      const response = await fetch(
-        'http://localhost:3000/quotes/random?limit=10'
-      );
+  const fetchQuotes = async () => {
+    try {
+      const response = await fetch(RANDOM_QUOTES_URL);
       const data = await response.json();
       setQuotes(data);
-    };
+    } catch (error) {
+      console.error('Error fetching quotes', error);
+    }
+  };
 
+  useEffect(() => {
     fetchQuotes();
   }, []);
 
@@ -22,6 +26,16 @@ export default function Home() {
       <h1 className="text-center text-3xl mb-6 dark:text-white">
         Quotes frontend app
       </h1>
+
+      <div className="text-center m-10">
+        <button
+          onClick={fetchQuotes}
+          className="px-6 py-3 text-xl bg-blue-900 text-white rounded-lg hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        >
+          Get Random Quotes
+        </button>
+      </div>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {quotes.map((quote) => (
           <div
