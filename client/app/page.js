dbea2874ba1fed0d55d404f812303205ff037dd4
AnthonyPriceOne2691 <anthonypriceone@gmail.com>
2024-10-28 22:57:25 +0300

    Displaying 10 random quotes when app loads

--- client/app/page.js
+++ client/app/page.js
@@ -1,3 +1,5 @@
+'use client';
+
 import { useEffect, useState } from 'react';
 
 export default function Home() {
@@ -17,17 +19,26 @@ export default function Home() {
 
   return (
     <div className="p-4">
-      <h1 className="text-center text-3xl mb-6">Quotes frontend app</h1>
+      <h1 className="text-center text-3xl mb-6 dark:text-white">
+        Quotes frontend app
+      </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {quotes.map((quote) => (
-          <div key={quote.id} className="bg-white p-4 shadow-md rounded-lg">
-            <p className="mb-4 text-lg italic">"{quote.text}"</p>
-            <p className="text-right font-semibold">— {quote.author}</p>
+          <div
+            key={quote.id}
+            className="bg-white dark:bg-gray-800 p-4 shadow-md rounded-lg"
+          >
+            <p className="mb-4 text-lg italic text-gray-900 dark:text-gray-100">
+              "{quote.text}"
+            </p>
+            <p className="mb-10 text-xl text-right font-semibold text-gray-700 dark:text-gray-300">
+              — {quote.author}
+            </p>
             <div className="flex flex-wrap mt-2">
               {quote.categories.map((category) => (
                 <span
                   key={category}
-                  className="text-xs bg-gray-200 text-gray-700 px-2 py-1 rounded-full mr-2 mb-2"
+                  className="text-lg bg-gray-200 dark:bg-gray-700 text-gray-700 dark:text-gray-300 px-2 py-1 rounded-full mr-2 mb-2"
                 >
                   {category}
                 </span>
