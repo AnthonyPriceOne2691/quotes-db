beeae903c827174dc83a23321b5b0bbb2a6d4c85
AnthonyPriceOne2691 <anthonypriceone@gmail.com>
2025-03-26 03:07:42 +0300

    Adaptive NavBar for mobile

--- client/app/_components/Navbar.jsx
+++ client/app/_components/Navbar.jsx
@@ -1,6 +1,43 @@
-import Link from 'next/link';
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const MenuButton = () => (
+    <button
+      onClick={toggleMenu}
+      className="ml-auto text-gray-800 dark:text-white focus:outline-none md:hidden"
+    >
+      <svg
+        className="w-6 h-6"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+        />
+      </svg>
+    </button>
+  );
+
+  const menuItems = [
+    { href: "/", text: "Random" },
+    { href: "/search", text: "Search" },
+    { href: "/quotes/create", text: "Create New" },
+  ];
+
   return (
     <nav className="bg-white dark:bg-gray-800 p-4 shadow-md">
       <div className="container mx-auto flex items-center">
@@ -10,25 +47,49 @@ export default function Navbar() {
           </h1>
         </Link>
 
-        <div className="text-xl pl-20 flex space-x-8">
-          <Link
-            href="/"
-            className="text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-400"
-          >
-            Random
-          </Link>
-          <Link
-            href="/search"
-            className="text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-400"
-          >
-            Search
-          </Link>
-          <Link
-            href="/quotes/create"
-            className="text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-400"
-          >
-            Create New
-          </Link>
+        {/* Hamburger Button for Mobile */}
+        <MenuButton />
+
+        {/* Mobile Menu */}
+        <div
+          className={`fixed top-0 right-0 h-full w-3/4 transform ${
+            isOpen ? "translate-x-0" : "translate-x-full"
+          } transition-transform duration-500 ease-in-out z-40 bg-gradient-to-r from-transparent to-white dark:from-transparent dark:to-gray-800`}
+        >
+          <div className="flex pr-5 pt-5 flex-col items-end justify-center space-y-6 text-2xl">
+            <MenuButton />
+            {menuItems.map((menuItem) => (
+              <Link
+                href={menuItem.href}
+                className="text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-400"
+                onClick={() => setIsOpen(false)}
+              >
+                {menuItem.text}
+              </Link>
+            ))}
+          </div>
+        </div>
+
+        {/* Background Overlay for Mobile Menu */}
+        {
+          <div
+            className={`${
+              isOpen ? "opacity-80 w-full" : "opacity-0 w-0 "
+            } transition-all duration-200 fixed h-full left-0 top-0 z-30 bg-black md:hidden`}
+            onClick={toggleMenu}
+          />
+        }
+
+        {/* Desktop Menu */}
+        <div className="hidden md:flex space-x-8 pl-20 text-xl">
+          {menuItems.map((menuItem) => (
+            <Link
+              href={menuItem.href}
+              className="text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-400"
+            >
+              {menuItem.text}
+            </Link>
+          ))}
         </div>
       </div>
     </nav>
